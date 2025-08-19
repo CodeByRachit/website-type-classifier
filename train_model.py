ec018f35ec0764ce1d5a8f30eaa785bd74164d8a
Rachit Yadav <rachit4yadav40@gmail.com>
2025-08-19 23:23:50 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -84,9 +84,9 @@ training_data_raw = [
     {"url": "https://www.wsj.com/news/world", "type": "news"},
     {"url": "https://www.usatoday.com/news/world/", "type": "news"},
     {"url": "https://www.cbsnews.com/world/", "type": "news"},
-    {"url": "https://www.abcnews.go.com/International/", "type": "news"},
     {"url": "https://www.nbcnews.com/world", "type": "news"},
     {"url": "https://www.theguardian.com/world", "type": "news"},
+    {"url": "https://www.abcnews.go.com/International/", "type": "news"},
     {"url": "https://www.independent.co.uk/world", "type": "news"},
 
     
@@ -557,6 +557,7 @@ else:
         print(f"Error: The following classes have fewer than 2 samples after scraping/filtering: {problematic_classes}. Cannot perform stratified split.")
         print("Please ensure each website type has at least two robust URLs in your training_data_raw.")
     print("--- Training Script Failed ---")
+
 
 
 
